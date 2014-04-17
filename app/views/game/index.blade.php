3c3f850747ddc4625679c582dd48692d8e0f64e3
Sajan Parikh <sajan@parikh.io>
2014-04-16 20:33:32 -0500

    High scorers.

--- app/views/game/index.blade.php
+++ app/views/game/index.blade.php
@@ -6,20 +6,36 @@
 	<table class="table table-hover table-bordered">
 		<thead>
 			<tr>
-				<th>#</th>
+				<th>Started</th>
 				<th>Player Count</th>
 				<th>Deal Count</th>
-				<th>Started</th>
+				<th>High Scorer</th>
 				<th>Actions</th>
 			</tr>
 		</thead>
 		<tbody>
 			@foreach($games as $game)
+			<?php
+
+				foreach ($game->players->all() as $player) {
+					$scores[$player->name] = $player->scores()->where('game_id', $game->id)->sum('amount');
+				}
+
+				arsort($scores);
+
+				$highScore = reset($scores);
+
+				$winners = array_keys($scores, $highScore);
+			?>
 				<tr>
-					<td>{{$game->id}}</td>
+					<td>{{$game->created_at->toDayDateTimeString()}}</td>
 					<td>{{$game->players->count()}}</td>
 					<td>{{$game->deals->count()}}</td>
-					<td>{{$game->created_at->toDayDateTimeString()}}</td>
+					<td>
+						@foreach($winners as $winner)
+							{{$winner}} <br>
+						@endforeach
+					</td>
 					<td>
 						{{HTML::linkRoute('game.show', 'View Game', $game->id, ['class' => 'btn btn-xs btn-primary'])}}
 					</td>
