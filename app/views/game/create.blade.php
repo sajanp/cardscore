709221f6e0289c321eddee49ce98e8aac2e6f0b4
Sajan Parikh <sajan@parikh.io>
2014-04-16 21:43:31 -0500

    Submit btn

--- app/views/game/create.blade.php
+++ app/views/game/create.blade.php
@@ -4,7 +4,7 @@
 	<h1>Start New Game</h1>
 	<p>Choose the players to start playing.</p>
 
-	{{Form::open(['route' => 'game.store'])}}
+	{{Form::open(['route' => 'game.store', 'id' => 'new-game-form'])}}
 		@foreach($players as $player)
 			<div class="form-group">
 				<div class="checkbox">
@@ -18,4 +18,21 @@
 
 		{{Form::submit('Start Game', ['class' => 'btn btn-success btn-lg'])}}
 	{{Form::close()}}
+@stop
+
+@section('footer-scripts')
+	<script>
+		$('#new-game-form').submit(function(){
+		    $('input[type=submit]').prop('disabled', true);
+		    $('input[type=submit]').prop('value', 'Working...');
+		});
+
+		$('#new-game-form').bind("keyup keypress", function(e) {
+			var code = e.keyCode || e.which; 
+			if (code  == 13) {               
+				e.preventDefault();
+				return false;
+			}
+		});
+	</script>
 @stop
