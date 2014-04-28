d525c9aee047c3d5aafd27655ad29299d2a5c85a
Sajan Parikh <sajan@parikh.io>
2014-04-28 01:51:52 -0500

    DB config and env.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('vor'),
-
-));
+$env = $app->detectEnvironment(function()
+{
+	return getenv('APP_ENV') ?: 'local';
+});
 
 /*
 |--------------------------------------------------------------------------
