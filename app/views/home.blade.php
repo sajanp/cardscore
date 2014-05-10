4f4f017000d3cd27267a1bcfed465ba6dc322dd7
Sajan Parikh <sajan@parikh.io>
2014-05-10 13:22:24 -0500

    change ordered to ul list.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,8 +5,8 @@
 	<h2>Changelog</h2>
 	<p class="lead">I'll note a list of changes I make here.  Both as record for myself, and in case you guys are curious.  If anyone of you are <i>really</i> curious, you can find my actual code and technical changelog <a href="http://git.sajan.io/sajan/cardscore-in/commits/master" target="_blank">here</a>.</p>
 
-	<ol>
+	<ul>
 		<li>Change delete button visibility from 5 minutes to 2 minutes.</li>
 		<li>Fixed calculation error of 'high scorer(s)' on the game list page.</li>
-	</ol>
+	</ul>
 @stop
