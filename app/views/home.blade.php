230a26ea51be5bc1591cb68185ecccba490254a3
Sajan Parikh <sajan@parikh.io>
2014-05-10 16:06:06 -0500

    some more stats on the homepage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,7 +10,7 @@
 
 	<div class="row">
 		<div class="col-md-2">
-			<h4># Hands Per Trump</h4>
+			<h4>Hands Per Trump</h4>
 			<table class="table table-condensed">
 				<thead>
 					<tr>
@@ -23,6 +23,58 @@
 					<tr>
 						<td>{{$trump->name}}</td>
 						<td>{{Deal::where('trump_id', $trump->id)->count()}}</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+
+		<div class="col-md-3">
+			<h4>Caller Wins Per Trump</h4>
+			<table class="table table-condensed">
+				<thead>
+					<tr>
+						<th>Trump</th>
+						<th>Caller Win %</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach(Trump::all() as $trump)
+					<tr>
+						<td>{{$trump->name}}</td>
+						<td>
+							@if(Deal::where('trump_id', $trump->id)->count())
+								{{number_format(Deal::where('trump_id', $trump->id)->where('acheived', true)->count() / Deal::where('trump_id', $trump->id)->count() * 100, 2)}}%
+							@else
+								N/A
+							@endif
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+
+		<div class="col-md-3">
+			<h4>Non-Caller Wins Per Trump</h4>
+			<table class="table table-condensed">
+				<thead>
+					<tr>
+						<th>Trump</th>
+						<th>Non-Caller Win %</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach(Trump::all() as $trump)
+					<tr>
+						<td>{{$trump->name}}</td>
+						<td>
+							@if(Deal::where('trump_id', $trump->id)->count())
+								{{number_format(Deal::where('trump_id', $trump->id)->where('acheived', false)->count() / Deal::where('trump_id', $trump->id)->count() * 100, 2)}}%
+							@else
+								N/A
+							@endif
+						</td>
 					</tr>
 					@endforeach
 				</tbody>
