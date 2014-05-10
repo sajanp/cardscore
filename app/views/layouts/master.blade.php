3a293349fe0bdc69c5a30d3a3d9edd2634b7b685
Sajan Parikh <sajan@parikh.io>
2014-05-10 18:37:31 -0500

    bring back default theme

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,6 +7,7 @@
 		<meta name=viewport content="width=device-width, initial-scale=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
 	</head>
 	<body>
