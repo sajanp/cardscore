beeb6eb94a7e17aa4172ae1d8eb4fb1b7cab3ac5
Sajan Parikh <sajan@parikh.io>
2014-05-10 13:21:39 -0500

    add changelog to homepage.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,6 +2,11 @@
 
 @section('body')
 	<h1>Rook Scoring System <small>by Sajan Parikh</small></h1>
-	<h2>Global Stats</h2>
-	<p class="lead">I'm planning on putting the global stats here.  Things like hands per suit, all time scores, games won per player, etc, etc.</p>
+	<h2>Changelog</h2>
+	<p class="lead">I'll note a list of changes I make here.  Both as record for myself, and in case you guys are curious.  If anyone of you are <i>really</i> curious, you can find my actual code and technical changelog <a href="http://git.sajan.io/sajan/cardscore-in/commits/master" target="_blank">here</a>.</p>
+
+	<ol>
+		<li>Change delete button visibility from 5 minutes to 2 minutes.</li>
+		<li>Fixed calculation error of 'high scorer(s)' on the game list page.</li>
+	</ol>
 @stop
