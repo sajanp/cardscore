740b9fe96f1f453cbf375fa3bb8271ea5cbe8390
Sajan Parikh <sajan@parikh.io>
2014-09-01 12:45:48 -0500

    change theme

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
 		<meta name=description content="">
 		<meta name=viewport content="width=device-width, initial-scale=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/lumen/bootstrap.min.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/sandstone/bootstrap.min.css">
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 		<style>
 			table tr td:last-child, table tr th:last-child {
