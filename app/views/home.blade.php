451f334eec1a255f75f0c2fc1053f7d05a6ce607
Sajan Parikh <sajan@parikh.io>
2014-09-01 13:41:13 -0500

    redo home

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.master')
 
 @section('body')
-	<div class="text-center">
-		<div class="btn-group">
-			<a href="#" class="btn btn-success">Contine Game In Progress</a>
-			{{HTML::linkRoute('game.create', 'New Game', null, ['class' => 'btn btn-primary'])}}
-		</div>
-	</div>
+	<p>{{HTML::linkRoute('game.create', 'New Game', null, ['class' => 'btn btn-primary'])}}</p>
 @stop
