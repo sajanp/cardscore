b08dbe437e56bcb7e247cb57a16adb3cee2f9dbf
Sajan Parikh <sajan@parikh.io>
2014-09-05 23:40:49 -0500

    changed theme

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
 		<meta name=description content="">
 		<meta name=viewport content="width=device-width, initial-scale=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/sandstone/bootstrap.min.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 		<style>
 			a:focus, a:active {
