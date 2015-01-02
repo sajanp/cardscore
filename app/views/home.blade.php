fb9b2f279d90dd959b1626fe877771bab7b9da89
Sajan Parikh <sajan@parikh.io>
2015-01-02 16:28:38 -0600

    remove message

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.master')
 
 @section('body')
-	<div class="alert alert-danger">
-		<p>I added the ability to add/remove points during the game and the location, but there was a small bug so it will not be fixed and added until after the new year.  Sorry.</p>
-	</div>
-
 	<p>{{HTML::linkRoute('game.create', 'New Game', null, ['class' => 'btn btn-primary'])}}</p>
 @stop
