44b6aeaedd914724cee187058094e7d55fb25a64
Sajan Parikh <sajan@parikh.io>
2015-03-29 17:35:55 -0500

    add player warning

--- app/views/player/create.blade.php
+++ app/views/player/create.blade.php
@@ -3,6 +3,11 @@
 @section('body')
 	<h1>Add New Player</h1>
 
+	<div class="alert alert-warning">
+	    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+	    <strong>WARNING!!!</strong> Make sure the player has not already been added, maybe you disabled the player a long time ago.
+	</div>
+
 	{{Form::open(['route' => 'player.store'])}}
 		<div class="form-group">
 			{{Form::label('name', 'Full Name')}}
